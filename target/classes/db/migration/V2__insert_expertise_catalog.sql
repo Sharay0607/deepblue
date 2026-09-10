89e456774fc6d7d9922982bb3cfe2536cdb65c5a
Juan Esteban <jebermudezo@unimagdalena.edu.co>
2026-09-10 18:50:43 -0500

    chore: dejar de rastrear target/ (carpeta de build de Maven)

--- target/classes/db/migration/V2__insert_expertise_catalog.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO expertise (name) VALUES
-    ('Marine Reptiles'),
-    ('Marine Mammals'),
-    ('Marine Birds'),
-    ('Trauma'),
-    ('Rehabilitation'),
-    ('Toxicology');
--- target/classes/db/migration/V3__add_tracking_device_to_animal.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE animals
-    ADD COLUMN tracking_device_code VARCHAR(50);
-
-ALTER TABLE animals
-    ADD CONSTRAINT uk_animals_tracking_device_code UNIQUE (tracking_device_code);
