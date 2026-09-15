61caac1626237554b4fc0cbd8a963375082ee0be
JuanEsBermudez26 <143522196+JuanEsBermudez26@users.noreply.github.com>
2026-09-14 19:43:46 -0500

    Se añadió Carpeta exception

---
+++ src/main/java/com/deepblue/rescue/exception/BusinessRuleException.java
@@ -0,0 +1,8 @@
+package com.deepblue.rescue.exception;
+
+public class BusinessRuleException extends RuntimeException {
+
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
